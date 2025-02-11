c2ca92cae583fc716e20a39fb13c6a6fc9e175f5
Codemonke123 <kadunayan86@gmail.com>
2025-02-11 14:58:23 +0530

    I Use the Scanner to read the Inputed String by me with the help of Scanner class

---
+++ Introduction.java
@@ -0,0 +1,19 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Introduction {
+
+    public static void main(String[] args) {
+        String name,work;
+        System.out.println("What is your name and what are you working on ?");
+        Scanner s = new Scanner(System.in);
+        name=s.nextLine();
+        work=s.nextLine();
+        System.out.println("\n");
+        System.out.println("-"+name);
+        System.out.println("-"+work);
+
+       
+    }
+}
