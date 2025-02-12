dd946056e164441a26ae161ae6342d30de97d9bb
Codemonke123 <kadunayan86@gmail.com>
2025-02-12 13:54:15 +0530

    Simple change

--- Introduction.java
+++ Introduction.java
@@ -6,6 +6,7 @@ public class Introduction {
 
     public static void main(String[] args) {
         String name,work;
+        
         System.out.println("What is your name and what are you working on ?");
         Scanner s = new Scanner(System.in);
         name=s.nextLine();
