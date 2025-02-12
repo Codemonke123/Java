4aea80c810b8646de724340fffdecbbb4fe71a4f
Codemonke123 <kadunayan86@gmail.com>
2025-02-12 14:05:52 +0530

    Just a change

--- Introduction.java
+++ Introduction.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class Introduction {
 
     public static void main(String[] args) {
-        String name,work;
-        
+        String name,work;        
         System.out.println("What is your name and what are you working on ?");
         Scanner s = new Scanner(System.in);
         name=s.nextLine();
